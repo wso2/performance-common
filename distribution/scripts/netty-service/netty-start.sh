27ae3d83238a7cd9fe90d7ba354469415248c421
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2017-08-03 20:03:02 +0530

    Avoid hanging the script in console

--- distribution/scripts/netty-service/netty-start.sh
+++ distribution/scripts/netty-service/netty-start.sh
@@ -30,13 +30,15 @@ if pgrep -f "$service_name" > /dev/null; then
     pkill -f $service_name
 fi
 
-if [[ -f $script_dir/logs/nettygc.log ]]; then
-    echo "GC Log exists. Moving to /tmp"
-    mv $script_dir/logs/nettygc.log /tmp/
+gc_log_file=$script_dir/logs/nettygc.log
+
+if [[ -f $gc_log_file ]]; then
+    echo "GC Log exists. Moving $gc_log_file to /tmp"
+    mv $gc_log_file /tmp/
 fi
 
 mkdir -p $script_dir/logs
 
 echo "Starting Netty"
-nohup java -Xms2g -Xmx2g -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$script_dir/logs/nettygc.log \
-    -jar $script_dir/$service_name-${performance.common.version}.jar --worker-threads 2000 --sleep-time $sleep_time &
+nohup java -Xms4g -Xmx4g -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$gc_log_file \
+    -jar $script_dir/$service_name-${performance.common.version}.jar --worker-threads 2000 --sleep-time $sleep_time > netty.out 2>&1 &
