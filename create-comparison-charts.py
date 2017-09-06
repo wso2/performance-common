d4b16ebd389e4370a6dfd16f00b0c23e15e0e34c
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2017-09-06 18:31:51 +0530

    Add more charts compare results with a given message size and backend delay. Filter errors. Save charts' details to a CSV file

--- create-comparison-charts.py
+++ create-comparison-charts.py
@@ -68,6 +68,8 @@ def add_suffix(string, suffix):
 print("Reading " + summary_files[0] + " with name " + names[0])
 # DataFrame to merge all data
 df = pd.read_csv(summary_files[0])
+# Filter errors
+df = df.loc[df['Error Count'] < 100]
 
 # DataFrame to append all data
 df_all = df.copy()
@@ -78,6 +80,8 @@ keys = ['Message Size (Bytes)', 'Sleep Time (ms)', 'Concurrent Users']
 for i in range(1, summary_count):
     print("Reading " + summary_files[i] + " with name " + names[i] + " to merge and append")
     df_read = pd.read_csv(summary_files[i])
+    # Filter errors
+    df_read = df_read.loc[df['Error Count'] < 100]
     if i == summary_count - 1:
         # Add suffixes to new right columns. Add suffixes to left columns using the first summary name
         suffixes = [add_suffix('', names[0]), add_suffix('', names[i])]
@@ -98,9 +102,15 @@ df['Message Size (Bytes)'] = df['Message Size (Bytes)'].map(apimchart.format_byt
 # Save lmplots
 xcolumns = ['Message Size (Bytes)', 'Sleep Time (ms)', 'Concurrent Users']
 xcharts = ['message_size', 'sleep_time', 'concurrent_users']
-ycolumns = ['Throughput', 'Average (ms)']
-ycharts = ['lmplot_througput', 'lmplot_average_time']
-ylabels = ['Throughput (Requests/sec)', 'Average Response Time (ms)']
+ycolumns = ['Throughput', 'Average (ms)', 'Max (ms)', '90th Percentile (ms)', '95th Percentile (ms)',
+            '99th Percentile (ms)', 'API Manager GC Throughput (%)', 'API Manager Load Average - Last 1 minute',
+            'API Manager Load Average - Last 5 minutes', 'API Manager Load Average - Last 15 minutes']
+ycharts = ['lmplot_throughput', 'lmplot_average_time', 'lmplot_max_time', 'lmplot_p90', 'lmplot_p95', 'lmplot_p99',
+           'lmplot_gc_throughput', 'lmplot_loadavg_1', 'lmplot_loadavg_5', 'lmplot_loadavg_15']
+ylabels = ['Throughput (Requests/sec)', 'Average Response Time (ms)', 'Maximum Response Time (ms)',
+           '90th Percentile (ms)', '95th Percentile (ms)', '99th Percentile (ms)', 'API Manager GC Throughput (%)',
+           'API Manager Load Average - Last 1 minute', 'API Manager Load Average - Last 5 minutes',
+           'API Manager Load Average - Last 15 minutes']
 
 for ycolumn, ylabel, ychart in zip(ycolumns, ylabels, ycharts):
     for xcolumn, xchart in zip(xcolumns, xcharts):
@@ -108,6 +118,25 @@ for ycolumn, ylabel, ychart in zip(ycolumns, ylabels, ycharts):
         title = ylabel + ' vs ' + xcolumn
         apimchart.save_lmplot(df_all, chart, xcolumn, ycolumn, title, ylabel=ylabel)
         apimchart.save_lmplot(df_all, chart + '_with_hue', xcolumn, ycolumn, title, hue='Name', ylabel=ylabel)
+
+unique_sleep_times_in_df_all = df_all['Sleep Time (ms)'].unique()
+unique_message_sizes_in_df_all = df_all['Message Size (Bytes)'].unique()
+
+for sleep_time in unique_sleep_times_in_df_all:
+    for message_size in unique_message_sizes_in_df_all:
+        df_filtered = df_all.loc[
+            (df_all['Message Size (Bytes)'] == message_size) & (df_all['Sleep Time (ms)'] == sleep_time)]
+        chart_suffix = '_' + apimchart.format_time(sleep_time) + '_' + apimchart.format_bytes(message_size)
+        title_suffix = ' vs Concurrent Users for ' + apimchart.format_bytes(
+            message_size) + ' messages with ' + apimchart.format_time(sleep_time) + ' backend delay'
+        ycolumns = ['Throughput', 'Average (ms)', 'Max (ms)', '90th Percentile (ms)', '95th Percentile (ms)',
+                    '99th Percentile (ms)', 'API Manager GC Throughput (%)']
+        charts = ['throughput', 'average_time', 'max_time', 'p90', 'p95', 'p99', 'gc_throughput']
+        ylabels = ['Throughput (Requests/sec)', 'Average Response Time (ms)', 'Maximum Response Time (ms)',
+                   '90th Percentile (ms)', '95th Percentile (ms)', '99th Percentile (ms)', 'GC Throughput (%)']
+        for ycolumn, ylabel, chart in zip(ycolumns, ylabels, charts):
+            apimchart.save_point_plot(df_filtered, chart + chart_suffix, 'Concurrent Users', ycolumn,
+                                      ylabel + title_suffix, hue='Name', ylabel=ylabel)
 
 
 def save_multi_columns_categorical_charts(chart, columns, y, hue, title, kind='point'):
@@ -119,9 +148,24 @@ def save_multi_columns_categorical_charts(chart, columns, y, hue, title, kind='p
                                                     len(columns) == 1, columns[0], kind)
 
 
-unique_sleep_times = df['Sleep Time (ms)'].unique()
+def save_bar_plot(chart, columns, y, hue, title):
+    comparison_columns = []
+    for column in columns:
+        for name in names:
+            comparison_columns.append(add_suffix(column, name))
+    df_results = df.loc[(df['Message Size (Bytes)'] == message_size) & (df['Sleep Time (ms)'] == sleep_time)]
+    all_columns = ['Message Size (Bytes)', 'Concurrent Users']
+    all_columns.extend(comparison_columns)
+    df_results = df_results[all_columns]
+    df_results = df_results.set_index(['Message Size (Bytes)', 'Concurrent Users']).stack().reset_index().rename(
+        columns={'level_2': hue, 0: y})
+    apimchart.save_bar_plot(df_results, chart, 'Concurrent Users', y, title, hue=hue)
 
-for sleep_time in unique_sleep_times:
+
+unique_sleep_times_in_df = df['Sleep Time (ms)'].unique()
+unique_message_sizes_in_df = df['Message Size (Bytes)'].unique()
+
+for sleep_time in unique_sleep_times_in_df:
     save_multi_columns_categorical_charts("comparison_thrpt", ['Throughput'],
                                           "Throughput (Requests/sec)", "API Manager",
                                           "Throughput vs Concurrent Users for " + str(sleep_time) + "ms backend delay")
@@ -147,5 +191,18 @@ for sleep_time in unique_sleep_times:
     save_multi_columns_categorical_charts("comparison_gc", ['API Manager GC Throughput (%)'],
                                           "GC Throughput (%)", "API Manager",
                                           "GC Throughput with " + str(sleep_time) + "ms backend delay")
+    for message_size in unique_message_sizes_in_df:
+        chart_suffix = '_' + apimchart.format_time(sleep_time) + '_' + message_size
+        title_suffix = " for " + message_size + " messages with " + apimchart.format_time(sleep_time) + " backend delay"
+        save_bar_plot('response_time' + chart_suffix,
+                      ['90th Percentile (ms)', '95th Percentile (ms)', '99th Percentile (ms)'],
+                      'Response Time (ms)', 'Summary',
+                      "Response Time Percentiles" + title_suffix)
+        save_bar_plot('loadavg' + chart_suffix,
+                      ['API Manager Load Average - Last 1 minute',
+                       'API Manager Load Average - Last 5 minutes',
+                       'API Manager Load Average - Last 15 minutes'],
+                      "Load Average", "API Manager",
+                      "Load Average" + title_suffix)
 
 print("Done")
