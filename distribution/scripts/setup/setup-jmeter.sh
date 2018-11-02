829c599051e490049c97a571dde3ce1cdc98b7b7
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2018-11-02 20:15:26 +0530

    Support Oracle JDK installation

--- distribution/scripts/setup/setup-jmeter.sh
+++ distribution/scripts/setup/setup-jmeter.sh
@@ -29,24 +29,34 @@ fi
 export script_name="$0"
 export script_dir=$(dirname "$0")
 export installation_dir=""
+export jmeter_dist=""
+export oracle_jdk_dist=""
 
 declare -a jmeter_plugins_array
 
 function usageCommand() {
-    echo "-i <installation_dir> [-j <jmeter_plugin>]"
+    echo "-i <installation_dir> -f <jmeter_dist> -d <oracle_jdk_dist> [-j <jmeter_plugin>]"
 }
 export -f usageCommand
 
 function usageHelp() {
-    echo "-i: The JMeter installation directory."
-    echo "-j: The JMeter plugin name. You can give multiple JMeter plugins to install."
+    echo "-i: Apache JMeter installation directory."
+    echo "-f: Apache JMeter tgz distribution."
+    echo "-d: Oracle JDK distribution. (If not provided, OpenJDK will be installed)"
+    echo "-j: Apache JMeter plugin name. You can give multiple JMeter plugins to install."
 }
 export -f usageHelp
 
-while getopts "gp:w:o:hi:j:" opt; do
+while getopts "gp:w:o:hi:f:d:j:" opt; do
     case "${opt}" in
     i)
         installation_dir=${OPTARG}
+        ;;
+    f)
+        jmeter_dist=${OPTARG}
+        ;;
+    d)
+        oracle_jdk_dist=${OPTARG}
         ;;
     j)
         jmeter_plugins_array+=("-p" "${OPTARG}")
@@ -64,6 +74,14 @@ function validate() {
         echo "Please provide the JMeter installation directory."
         exit 1
     fi
+    if [[ ! -f $jmeter_dist ]]; then
+        echo "Please specify the JMeter distribution file (*.tgz)"
+        exit 1
+    fi
+    if [[ ! $jmeter_dist =~ ^.*\.tgz$ ]]; then
+        echo "Please provide the JMeter tgz distribution file (*.tgz)"
+        exit 1
+    fi
 }
 export -f validate
 
@@ -72,9 +90,19 @@ export jmeter_plugins="${jmeter_plugins_array[*]}"
 
 function setup() {
     declare -a jmeter_plugins_array=($jmeter_plugins)
+
+    if [[ -f $oracle_jdk_dist ]]; then
+        echo "Installing Oracle JDK from $oracle_jdk_dist"
+        $script_dir/../java/install-java.sh -f $oracle_jdk_dist
+    fi
+
     echo "Setting up JMeter in $installation_dir"
-    $script_dir/../jmeter/install-jmeter.sh -d -i $installation_dir "${jmeter_plugins_array[@]}"
+    $script_dir/../jmeter/install-jmeter.sh -f $jmeter_dist -i $installation_dir "${jmeter_plugins_array[@]}"
 }
 export -f setup
 
-$script_dir/setup-common.sh "${opts[@]}" "$@" -p openjdk-8-jdk -p zip -p jq
+if [[ ! -f $oracle_jdk_dist ]]; then
+    SETUP_COMMON_ARGS+="-p openjdk-8-jdk"
+fi
+
+$script_dir/setup-common.sh "${opts[@]}" "$@" $SETUP_COMMON_ARGS -p zip -p unzip -p jq
